1a37304a8275cc3b8c9ba01529264428118f846f
Muscar1a <vn.annguyenit@gmail.com>
2023-12-14 17:01:47 +0700

    add sorting

---
+++ Heuristic/sorting.hpp
@@ -0,0 +1,69 @@
+#include "declaration.hpp"
+
+// ! *********************************************************************** ! //
+
+void merge(population array[], int const left, int const mid, int const right) {
+    int const subArrayOne = mid - left + 1;
+    int const subArrayTwo = right - mid;
+ 
+    // Create temp arrays
+    population *leftArray = new population[subArrayOne],
+         *rightArray = new population[subArrayTwo];
+ 
+    // Copy data to temp arrays leftArray[] and rightArray[]
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i].biggest = array[left + i].biggest;
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j].biggest = array[mid + 1 + j].biggest;
+ 
+    auto indexOfSubArrayOne = 0, indexOfSubArrayTwo = 0;
+    int indexOfMergedArray = left;
+ 
+    // Merge the temp arrays back into array[left..right]
+    while (indexOfSubArrayOne < subArrayOne
+           && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne].biggest >= rightArray[indexOfSubArrayTwo].biggest) {
+            //* sort in descending order
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+ 
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray]
+            = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+ 
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray]
+            = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+    delete[] leftArray;
+    delete[] rightArray;
+}
+ 
+// begin is for left index and end is right index
+// of the sub-array of arr to be sorted
+void mergeSort(population array[], int const begin, int const end)
+{
+    if (begin >= end)
+        return;
+ 
+    int mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
