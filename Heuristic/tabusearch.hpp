935923a4c2ab76f6280a70d2992d0a1107a7097d
Muscar1a <vn.annguyenit@gmail.com>
2023-12-17 17:22:20 +0700

    Editing tabu search

--- Heuristic/tabusearch.hpp
+++ Heuristic/tabusearch.hpp
@@ -2,6 +2,24 @@
 
 // ! *********************************************************************** ! //
 
+//! haven't create initial solution yet
+bool cmp(vector<pii> a, vector<pii> b) {
+    int suma = 0, sumb = 0;
+    for(int i = 1; i < a.size(); i++) {
+        if(a[i].first <= N && a[i].first >= 1) {
+            if(a[i - 1].first <= N && a[i - 1].first >= 1)
+                suma += d[a[i - 1].second][a[i].first];    
+            else 
+                suma += d[a[i - 1].first][a[i].first];
+            suma += d[a[i].first][a[i].second];
+        } else {
+            suma += d[a[i - 1].first][a[i].first];
+        }
+    }
+    for(int i = 1; i < b.size(); i++) {
+    }
+}
+
 vector<vector<pii>> get_neighbor(vector<pii> current_sol) {
     vector<vector<pii>> neighborhood;
     for(int i = 0; i < current_sol.size(); i++) {
@@ -13,11 +31,14 @@ vector<vector<pii>> get_neighbor(vector<pii> current_sol) {
     }
     return neighborhood;
 }
+
+
 void tabu_search() {
     vector<pii> current_sol;
     vector<pii> best_sol, tabu_list;
     
     for(int iteration = 0; iteration < 1000; iteration++) {
         vector<vector<pii>> neighbor = get_neighbor(current_sol);
+        
     }
 }
