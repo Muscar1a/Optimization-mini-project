19151089dcc7d2afe522a3f53116e309f7a15dd5
Muscar1a <vn.annguyenit@gmail.com>
2023-12-21 16:49:51 +0700

    Complete GA, but no mutation

--- Heuristic/GA.hpp
+++ Heuristic/GA.hpp
@@ -66,6 +66,17 @@ vector<vector<pii>> create_full_route(vector<int> route) {
     return full_pop;
 }
 
+int return_biggest_fitness_each_config(vector<int> child) {
+    vector<vector<pii>> meo = create_full_route(child);
+    int biggest_fitness = 0;
+    for(auto x:meo) {
+        int tmp = tabu_search_return_fitness(x);
+        biggest_fitness = max(biggest_fitness, tmp);
+    }
+    return biggest_fitness;
+}
+
+//* createPopulation is ok
 void createPopulation() {
     // cerr << "Create population\n";
     vector<int> t;
@@ -126,41 +137,48 @@ void createPopulation() {
         pop[x].biggest_fitness_among_routes = biggest_fitness;
         // cout << pop[x].biggest_fitness_among_routes << '\n';
     }
-    /*
+    /* 
     for(int p = 0; p < SIZEofPopulation; p++) {
         cout << "p = " << p + 1 << " - " << pop[p].biggest_fitness_among_routes << ":\n";
         for(auto i:pop[p].route) cout << i << ' ';
         cout << " - " << pop[p].biggest_fitness_among_routes << '\n';
     
     }
-    */
+     */
 }
 
-//? need to check
+//* tournament is ok
 population tournament(vector<population> pop, int tournament_size) {
-    cerr << "Tournament\n";
+    //!cerr << "Tournament\n";
+    //!cerr << pop.size() << '\n';
     population select;
-    bool smallest_fitness = INT_MAX;
-    //vector<int> prepare_to_del;
+    int smallest_fitness = INT_MAX;
     for(int p = 0; p < tournament_size; p++) {
         int random_index = Rand(0, pop.size() - 1); 
-        //prepare_to_del.push_back(random_index);
         population par = pop[random_index];
+        //!for(auto i:par.route) cerr << i << " ";
+        //!cerr << " - " << par.biggest_fitness_among_routes << '\n';
         if(smallest_fitness > par.biggest_fitness_among_routes) {
+            //!REACH;
             smallest_fitness = par.biggest_fitness_among_routes;
             select = par;
         }
     }
+    // cerr << "The selected parent\n";
+    // for(auto i:select.route) cerr << i << " ";
     //* after a while for searching, it seems that no need to reduce the size of populations
     //! after tournament step, the size of population will be reduced
     /*sort(prepare_to_del.begin(), prepare_to_del.end(), greater<int>());
     for(auto i:prepare_to_del) {
         pop.erase(pop.begin() + i);
     }*/
-    
+    //!cerr << "Selected chromosome after tournament:\n";
+    //!for(auto i:select.route) cerr << i << ' ';
+    //!cerr << " - " << select.biggest_fitness_among_routes << '\n';
     return select;    
 }
 
+//* making_children is ok
 pair<vector<int>, vector<int>> making_children(vector<int> par1, vector<int> par2) {
     int sz = par1.size();
     int point1 = sz / 3;
@@ -198,47 +216,93 @@ pair<vector<int>, vector<int>> making_children(vector<int> par1, vector<int> par
     return {child1, child2};
 }
 
+//* ok
 vector<population> crossover(vector<population> parents, double crossover_rate) {
+    //!cerr << "Cross over step\n";
     vector<population> offspring, par = parents;
     vector<int> parent_index(parents.size());
     iota(parent_index.begin(), parent_index.end(), 0);
 
     while(true) {
+        //!cerr << "Size of parent_index = " << parent_index.size() << '\n';
         if(parent_index.size() == 0) break;
-        if(parents.size() == 1) {
+        if(parent_index.size() == 1) {
             offspring.push_back(par[parent_index[0]]);
             break;
         }
         int par_index_0 = Rand(0, parent_index.size() - 1);
         int par_index_1 = Rand(0, parent_index.size() - 1);
-        
+        while(par_index_0 == par_index_1) {
+            par_index_1 = Rand(0, parent_index.size() - 1);
+        }
+        //!cerr << par_index_0 << ' ' << par_index_1 << '\n';
         if(par_index_0 > par_index_1) swap(par_index_0, par_index_1);
         parent_index.erase(parent_index.begin() + par_index_1);
         parent_index.erase(parent_index.begin() + par_index_0);
         
-        vector<int> par_0 = par[par_index_0].route, par_1 = par[par_index_1].route;
+        population par_0 = par[par_index_0], par_1 = par[par_index_1];
         //vector<int> child_0 = par_0, child_1 = par_1;
 
         double r = double(Rand(0, 100)) * 1.0 / 100.0;
+        //!cerr << "rate = " << r << '\n';
         if(r < crossover_rate) {
-            offspring.push_back({par_0, 0});
-            offspring.push_back({par_1, 0});
+           //! cerr << "Not crosver\n";
+            // if we push the par again, their biggest_fitness_among_routes are still the same
+            offspring.push_back(par_0);
+            offspring.push_back(par_1);
         } else {
-            pair<vector<int>, vector<int>> children = making_children(par_0, par_1); 
-            offspring.push_back({children.first, 0});
-            offspring.push_back({children.second, 0});
+            //!cerr << "Crossover\n";
+            // when we cross over, we need to calculate the biggest_fitness_among_routes of their children
+            pair<vector<int>, vector<int>> children = making_children(par_0.route, par_1.route); 
+            int t1 = return_biggest_fitness_each_config(children.first);
+            int t2 = return_biggest_fitness_each_config(children.second);
+            offspring.push_back({children.first, t1});
+            offspring.push_back({children.second, t2});
         }
     }
     return offspring;
 }
 
+//! haven't used yet
+vector<population> mutate(vector<population> chromesomes, double mutation_rate) {
+   //! cerr << "Mutation step\n";
+    vector<population> mutated_chromesomes;
+    for(int i = 0; i < chromesomes.size(); i++) {
+        double r = double(Rand(0, 100)) * 1.0 / 100.0;
+        if(r <= mutation_rate) {
+            // when their is a mutation occurs, we also need to calculate its biggeqst_fitness_among_routes again
+            int random_index = Rand(1, chromesomes[i].route.size() - 2);
+            int random_index_2 = Rand(1, chromesomes[i].route.size() - 2);
+            swap(chromesomes[i].route[random_index], chromesomes[i].route[random_index_2]);
+            chromesomes[i].biggest_fitness_among_routes = return_biggest_fitness_each_config(chromesomes[i].route);
+        }
+    }
+    return mutated_chromesomes;
+}
+
+//* looks ok
 vector<population> next_generation(vector<population> pop, int tournament_size, double mutation_rate, double crossover_rate) {
-    cerr << "Next generation operation\n";
+    // cerr << "Next generation operation\n";
     vector<population> selected_parents;
     for(int n = 0; n < pop.size(); n++) {
         population selected_parent = tournament(pop, tournament_size);
         selected_parents.push_back(selected_parent);
     }
-   // crossover(selected_parents, crossover_rate);
-    return selected_parents;
+   //! cerr << '\n' << "Size of the selected parents in next_gen operation: " << selected_parents.size() << '\n';
+    vector<population> next_gen;
+    next_gen = crossover(selected_parents, crossover_rate);
+    //next_gen = mutate(next_gen, mutation_rate);
+    return next_gen;
+}
+
+population evaluate(vector<population> pop) {
+    population success;
+    int biggest = INT_MAX;
+    for(int i = 0; i < pop.size(); i++) {
+        if(pop[i].biggest_fitness_among_routes < biggest) {
+            success = pop[i];
+            biggest = pop[i].biggest_fitness_among_routes;
+        }
+    }
+    return success;
 }
